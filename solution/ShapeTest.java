40764bf44466789ef95294488c1cc2eeb9f65170
bastienwcs <bastien@wildcodeschool.fr>
2018-06-05 10:27:02 +0200

    Fix

--- solution/ShapeTest.java
+++ solution/ShapeTest.java
@@ -1,31 +1,56 @@
 import junit.framework.*;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class ShapeTest extends TestCase {
-	@Test
-	public void testTrue() throws Exception {
-		assertEquals(true, true);
-	}
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Random;
 
-	@Test
-	public void testRectangle() throws Exception {
-		Rectangle rectangle = new Rectangle(6, 4);
-		assertEquals(20.0, rectangle.perimeter());
-		assertEquals(24.0, rectangle.area());
-		assertEquals(false, rectangle.isSquare());
-	}
+public class ShapeTest {
 
-	@Test
-	public void testCarre() throws Exception {
-		Rectangle carre = new Rectangle(5, 5);
-		assertEquals(20.0, carre.perimeter());
-		assertEquals(true, carre.isSquare());
-	}
+    @Test
+    public void ShapesAreSortableOnArea() {
+        // Arrange
+        double area, side, radius, base, height, width;
+        ArrayList<Shape> expected = new ArrayList<Shape>();
 
-	@Test
-	public void testCircle() throws Exception {
-		Circle circle = new Circle(5);
-		assertEquals(10 * Math.PI, circle.perimeter());
-		assertEquals(25 * Math.PI, circle.area());
-	}
+        side = 1.1234;
+        expected.add(new Square(side));
+
+        radius = 1.1234;
+        expected.add(new Circle(radius));
+
+        height = 2.;
+        base = 5.;
+        expected.add(new Triangle(base, height));
+
+        height = 3.;
+        base = 4.;
+        expected.add(new Triangle(base, height));
+
+        width = 4.;
+        expected.add(new Rectangle(width, height));
+
+        Random random = new Random();
+        ArrayList<Shape> actual = createRandomOrderedList(random, expected);
+
+        // Act
+        Collections.sort(actual);
+
+        // Assert
+        Iterator a = actual.iterator();
+        for (Shape e : expected) {
+            Assert.assertEquals(e, a.next());
+        }
+    }
+
+    private ArrayList<Shape> createRandomOrderedList(Random random, ArrayList<Shape> expected) {
+        ArrayList<Shape> actual = new ArrayList<Shape>();
+        for (Shape shape : expected) {
+            int j = random.nextInt(actual.size() + 1);
+            actual.add(j, shape);
+        }
+        return actual;
+    }
 }
